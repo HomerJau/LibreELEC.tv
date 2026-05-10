c1b447cc6198d4caa30a5f474565aefa07376c7b
HomerJau <Garry.Hobman@reva.net.au>
2026-05-11 06:03:55 +1000

    Python Bump to 3.11.15

--- packages/lang/Python3/package.mk
+++ packages/lang/Python3/package.mk
@@ -3,8 +3,8 @@
 
 PKG_NAME="Python3"
 # When changing PKG_VERSION remember to sync PKG_PYTHON_VERSION!
-PKG_VERSION="3.11.13"
-PKG_SHA256="8fb5f9fbc7609fa822cb31549884575db7fd9657cbffb89510b5d7975963a83a"
+PKG_VERSION="3.11.15"
+PKG_SHA256="272179ddd9a2e41a0fc8e42e33dfbdca0b3711aa5abf372d3f2d51543d09b625"
 PKG_LICENSE="OSS"
 PKG_SITE="https://www.python.org/"
 PKG_URL="https://www.python.org/ftp/python/${PKG_VERSION}/${PKG_NAME::-1}-${PKG_VERSION}.tar.xz"
